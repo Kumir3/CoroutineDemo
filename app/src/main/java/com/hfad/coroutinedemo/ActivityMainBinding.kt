d0a0d3ea04431b78fb9414ad9acd03a1a8906e25
Kirill Raz <rkirill2002@Mail.ru>
2023-11-15 20:21:39 +0700

    Initial commit

---
+++ app/src/main/java/com/hfad/coroutinedemo/ActivityMainBinding.kt
@@ -0,0 +1,5 @@
+package com.hfad.coroutinedemo
+
+class ActivityMainBinding {
+
+}
---
+++ app/src/main/java/com/hfad/coroutinedemo/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.hfad.coroutinedemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var count: Int = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.seekBar.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar,
+                                           progress: Int, fromUser: Boolean) {
+                count = progress
+                binding.countText.text = "${count} coroutines"
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+            }
+        })
+    }
